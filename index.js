56e8869ce63e17bec0f436843ec4343cd102e3bd
Martin Rosenberg <martin.rosenberg@strv.com>
2015-12-07 11:19:21 +0100

    Build fix

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var Calendar = require('./src/Calendar');
-
-ReactDOM.render(
-  <div>
-    <h1>Calendar example</h1>
-    <Calendar />
-  </div>,
-  document.getElementById('root')
-);
+module.exports = require('./dist/react-form-calendar');
